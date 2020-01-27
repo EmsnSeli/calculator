4e52d6cf7c05795165c2d29bff9ef19b828a11f3
Emir Selimovic <emir.selimovichtl@gmail.com>
2020-01-27 23:24:57 +0100

    number input works with operators

--- script/script.js
+++ script/script.js
@@ -10,15 +10,38 @@ let pCurrPath = document.querySelector(".currentPath p");
 //get p of cur typing
 let pCurrTyping = document.querySelector(".currentInput p");
 
+let operatorPressed = false;
+let currTypingValue = "";
+
 numberButtons.forEach(numberButton => {
-    numberButton.addEventListener("click", function() {
-        pCurrTyping.textContent += numberButton.value;
+    numberButton.addEventListener("click", function () {
+        if (pCurrTyping.textContent.length < 11) {
+            if (operatorPressed) {
+                pCurrTyping.textContent = "";
+                pCurrTyping.textContent += numberButton.value;
+                operatorPressed = false;
+            } else
+                pCurrTyping.textContent += numberButton.value;
+            currTypingValue = pCurrTyping.textContent;
+        }
     })
 });
 
 operators.forEach(operator => {
-    operator.addEventListener("click", function() {
-        // pCurrTyping.textContent += numberButton.
-        console.log(operator.className);
+    operator.addEventListener("click", function () {
+        if(!pCurrTyping.textContent) return;
+        pCurrPath.textContent += currTypingValue + operator.value;
+        operatorPressed = true;
+        operatorValue = operator.value;
+        console.log(operator.className + operator.value);
     })
 });
+
+//operator functions
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let divide = (a, b) => {
+    if (b == 0) return "Error";
+    return a / b;
+}
+let multiply = (a, b) => a * b;

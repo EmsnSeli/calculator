b199a9ad150ac2cffdcb675ad641d5dac84a1b43
Emir Selimovic <emir@freshfx.at>
2020-01-28 14:11:52 +0100

    cant type more than one zero at beginning, added backspace DEL

--- script/script.js
+++ script/script.js
@@ -3,6 +3,8 @@ let operators = document.querySelectorAll(".operator");
 let equalsButton = document.querySelector("#equlasbutton");
 let numberButtons = document.querySelectorAll("button:not(.operator):not(#equlasbutton):not(.clearbutton):not(.comma)");
 let commabutton = document.querySelector(".comma");
+let clearButton = document.querySelector(".clearbutton");
+let delButton = document.querySelector("#backspace");
 
 //get p of path writing
 let pCurrPath = document.querySelector(".currentPath span");
@@ -19,13 +21,23 @@ let equalsPressed = false;
 //loop over every numberpad and add eventlistener
 numberButtons.forEach(numberButton => {
     numberButton.addEventListener("click", function() {
+        if (equalsPressed) {
+            pCurrTyping.textContent = "";
+            pCurrPath.textContent = "";
+            equalsPressed = false;
+        }
         if (pCurrTyping.textContent.length < 11) {
             if (operatorPressed) {
                 pCurrTyping.textContent = "";
                 pCurrTyping.textContent += numberButton.value;
                 operatorPressed = false;
-            } else
+            } else {
                 pCurrTyping.textContent += numberButton.value;
+                if (pCurrTyping.textContent[0] == "0" && numberButton.value == "0" && !pCurrTyping.textContent.includes(".")) {
+                    pCurrTyping.textContent = "0";
+                    return;
+                }
+            }
             currTypingValue = pCurrTyping.textContent;
         }
     })
@@ -34,11 +46,11 @@ numberButtons.forEach(numberButton => {
 //loop over every operator and add click eventlistener 
 operators.forEach(operator => {
     operator.addEventListener("click", function() {
+        if (equalsPressed) return;
         if (!pCurrTyping.textContent) return;
         pCurrPath.textContent += currTypingValue + " " + operator.value + " ";
         operatorPressed = true;
         commaPressed = false;
-        console.log(operator.className + operator.value);
     })
 });
 
@@ -53,6 +65,7 @@ let multiply = (a, b) => a * b;
 
 //when equals button is pressed resolve the equation
 equalsButton.addEventListener("click", function() {
+    if (equalsPressed) return;
     pCurrPath.textContent += currTypingValue;
     let evalVal = eval(pCurrPath.textContent);
     pCurrPath.textContent += " =";
@@ -62,7 +75,22 @@ equalsButton.addEventListener("click", function() {
 
 //add comma when pressed only once
 commabutton.addEventListener("click", function() {
-    if (commaPressed) return;
+    if (pCurrTyping.textContent.includes(".") || operatorPressed) return;
     commaPressed = true;
     pCurrTyping.textContent += ".";
+})
+
+//clearbutton handling
+clearButton.addEventListener("click", function() {
+    pCurrPath.textContent = "";
+    pCurrTyping.textContent = "";
+    commaPressed = false;
+    equalsPressed = false;
+    operatorPressed = false;
+})
+
+//del button handling
+delButton.addEventListener("click", function() {
+    if (pCurrTyping.textContent.length > 0)
+        pCurrTyping.textContent = pCurrTyping.textContent.slice(0, -1);
 })
